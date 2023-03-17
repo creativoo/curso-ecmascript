fb65a43a7a7744d006186546b6710c4418b0a9fa
creativoo <creativoo@creativoo>
2023-03-17 14:18:10 -0700

    agregando actualizacion es12

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,4 @@
+const string = 'JavaScript es un maravilloso lenguaje de programacion';
+const replacedString = string.replace("JavaScript", "TypeScript");  // remplaza la palabra "JavaScript" por "TypeScript".
+
+console.log(replacedString);
